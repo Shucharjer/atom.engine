71646db7c49269e1b89144681550a8d0007a160c
Shucharjer <Shucharjer@outlook.com>
2025-04-06 00:09:09 +0800

    update Mesh

--- content/include/systems/render/Mesh.hpp
+++ content/include/systems/render/Mesh.hpp
@@ -1,11 +1,13 @@
 #pragma once
 #include <cstdint>
 #include <containers.hpp>
-#include <pch.hpp>
 #include "Material.hpp"
+#include "VertexArrayObject.hpp"
+#include "pchs/std.hpp"
+#include "systems/render/BufferObject.hpp"
 #include "systems/render/Material.hpp"
+#include "systems/render/ShaderProgram.hpp"
 #include "systems/render/Vertex.hpp"
-
 
 namespace atom::engine::systems::render {
 
@@ -18,9 +20,16 @@ enum class CullingMode : uint8_t {
 struct Mesh {
     bool visibility     = true;
     CullingMode culling = CullingMode::Back;
+    VertexArrayObject vao;
+    VertexBufferObject vbo;
     atom::vector<Vertex> vertices;
     atom::vector<uint32_t> indices;
     atom::vector<Material> materials;
+
+    Mesh() = default;
+    Mesh(Mesh&&) noexcept;
+
+    void draw(const ShaderProgram& program) const noexcept;
 };
 
 } // namespace atom::engine::systems::render
--- content/src/systems/render/Mesh.cpp
+++ content/src/systems/render/Mesh.cpp
@@ -1,5 +1,21 @@
 #include "systems/render/Mesh.hpp"
+#include "pchs/graphics.hpp"
+
+#include "systems/render/ShaderProgram.hpp"
 
 namespace atom::engine::systems::render {
 
+Mesh::Mesh(Mesh&& that) noexcept
+    : visibility(that.visibility), culling(that.culling), vao(std::move(that.vao)),
+      vbo(std::move(that.vbo)), vertices(std::move(that.vertices)),
+      indices(std::move(that.indices)), materials(std::move(that.materials)) {}
+
+void Mesh::draw(const ShaderProgram& program) const noexcept {
+
+    vao.bind();
+    glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(indices.size()), GL_UNSIGNED_INT, 0);
+    vao.unbind();
+    glActiveTexture(0);
 }
+
+} // namespace atom::engine::systems::render

fda6f3487b3cc95a3e0919685ef932e0aeef20e3
Shucharjer <Shucharjer@outlook.com>
2025-04-06 00:16:04 +0800

    add base for key frame

--- content/include/systems/render/Animation.hpp
+++ content/include/systems/render/Animation.hpp
@@ -1,9 +1,23 @@
 #pragma once
+#include "pchs/math.hpp"
 
 namespace atom::engine::systems::render {
 
-class Animation {
-
+struct KeyPosition {
+    math::Vector3 position;
+    float timeStamp;
 };
 
-}
+struct KeyRotation {
+    math::Quaternion orientation;
+    float timeStamp;
+};
+
+struct KeyScale {
+    math::Vector3 scale;
+    float timeStamp;
+};
+
+class Animation {};
+
+} // namespace atom::engine::systems::render

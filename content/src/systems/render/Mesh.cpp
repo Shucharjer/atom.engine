3cb3d1ca7c677d3c3c1724f3d5d1d274e76019e0
Shucharjer <Shucharjer@outlook.com>
2025-04-16 10:55:10 +0800

    add active texture before drawing

--- content/src/systems/render/Mesh.cpp
+++ content/src/systems/render/Mesh.cpp
@@ -1,7 +1,9 @@
 #include "systems/render/Mesh.hpp"
+#include <asset.hpp>
 #include "pchs/graphics.hpp"
 
 #include "systems/render/ShaderProgram.hpp"
+#include "systems/render/Texture.hpp"
 
 namespace atom::engine::systems::render {
 
@@ -10,7 +12,31 @@ Mesh::Mesh(Mesh&& that) noexcept
       vbo(std::move(that.vbo)), vertices(std::move(that.vertices)),
       indices(std::move(that.indices)), materials(std::move(that.materials)) {}
 
+static void TryActiveTexture(
+    hub& hub,
+    library<Texture>& library,
+    const ShaderProgram& program,
+    const Texture& texture,
+    uint16_t& i
+) {
+    if (texture) {
+        auto handle = texture.get_handle();
+        auto proxy  = library.fetch(handle);
+        glActiveTexture(GL_TEXTURE0 + i++);
+        glBindTexture(GL_TEXTURE_2D, proxy->m_Id);
+    }
+}
+
 void Mesh::draw(const ShaderProgram& program) const noexcept {
+
+    auto& hub     = hub::instance();
+    auto& library = hub.library<Texture>();
+    if (!materials.empty()) {
+        auto& material = materials[0];
+        uint16_t i     = 0;
+        TryActiveTexture(hub, library, program, material.baseColorTexture, i);
+        TryActiveTexture(hub, library, program, material.ambientTexture, i);
+    }
 
     vao.bind();
     glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(indices.size()), GL_UNSIGNED_INT, 0);

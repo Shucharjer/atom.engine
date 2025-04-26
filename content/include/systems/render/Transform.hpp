1dc8b9ec20cf497e93b32fbe8d4391f3988f87d6
Shucharjer <Shucharjer@outlook.com>
2025-04-26 15:07:14 +0800

    add a function convert transform to matrix

--- content/include/systems/render/Transform.hpp
+++ content/include/systems/render/Transform.hpp
@@ -7,6 +7,11 @@ struct Transform {
     math::Vector3 position;
     math::Quaternion rotation;
     math::Vector3 scaling;
+
+    [[nodiscard]] math::Mat4 toMatrix() const noexcept {
+        return glm::translate(math::Mat4(1.0F), position) * glm::toMat4(rotation) *
+               glm::scale(math::Mat4(1.0F), scaling);
+    }
 };
 
 } // namespace atom::engine::systems::render

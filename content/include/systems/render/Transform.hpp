c06ec5fb9d73e058b2f9f087160e33788212b0c2
Shucharjer <Shucharjer@outlook.com>
2025-05-04 19:49:40 +0800

    [render] set default scaling in transform

--- content/include/systems/render/Transform.hpp
+++ content/include/systems/render/Transform.hpp
@@ -6,7 +6,7 @@ namespace atom::engine::systems::render {
 struct Transform {
     math::Vector3 position;
     math::Quaternion rotation;
-    math::Vector3 scaling;
+    math::Vector3 scaling{ 1.0F, 1.0F, 1.0F };
 
     [[nodiscard]] math::Mat4 toMatrix() const noexcept {
         return glm::translate(math::Mat4(1.0F), position) * glm::toMat4(rotation) *

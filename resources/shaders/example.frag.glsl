941a2495e511aaadc5e07293bd88036c66751774
Shucharjer <Shucharjer@outlook.com>
2025-05-04 21:27:01 +0800

    update example shader

--- resources/shaders/example.frag.glsl
+++ resources/shaders/example.frag.glsl
@@ -1,7 +1,37 @@
 #version 460 core
 out vec4 FragColor;
 
+in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoords;
+
+struct Material {
+    vec4 baseColorFactor;
+    float metallicFactor;
+    float roughnessFactor;
+    float ambientFactor;
+    vec3 emissiveFactor;
+    vec3 ambientLight;
+
+    sampler2D baseColorTexture;
+    sampler2D ambientTexture;
+    sampler2D diffuseTexture;
+    sampler2D specularTexture;
+    sampler2D emissionTexture;
+    sampler2D metallicTexture;
+    sampler2D roughnessTexture;
+    sampler2D occlusionTexture;
+};
+
+uniform vec3 viewPos;
+uniform Material material;
+
 void main()
 {
-    FragColor = vec4(1.0, 1.0, 0, 1.0);
+    vec4 ambientColor = texture(material.ambientTexture, TexCoords);
+    vec4 diffuseColor = texture(material.diffuseTexture, TexCoords);
+    vec4 specularColor = texture(material.specularTexture, TexCoords);
+    vec4 emissionColor = texture(material.emissionTexture, TexCoords);
+
+    FragColor = diffuseColor;
 }
--- resources/shaders/example.vert.glsl
+++ resources/shaders/example.vert.glsl
@@ -1,13 +1,12 @@
 #version 460 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
-layout (location = 2) in vec3 aTexCoords;
+layout (location = 2) in vec2 aTexCoords;
 
 out vec3 FragPos;
-out vec3 TexCoords;
 out vec3 Normal;
+out vec2 TexCoords;
 
-uniform vec3 viewPos;
 uniform mat4 view;
 uniform mat4 proj;
 uniform mat4 model;

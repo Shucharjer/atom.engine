a85812067d64ecc0eae6d9132e7c43867570ceb7
Shucharjer <Shucharjer@outlook.com>
2025-05-05 01:24:17 +0800

    [test] update keyboard callback

--- test/src/KeyboardCallback.cpp
+++ test/src/KeyboardCallback.cpp
@@ -8,18 +8,16 @@
 
 using namespace atom;
 
-const float delta_time = 0.00694F; // 144 FPS
 const float move_speed = 5.0F;
 
 void MoveForward() {
-    auto command = gWorld->command();
-    auto querier = gWorld->query();
-
+    auto command    = gWorld->command();
+    auto querier    = gWorld->query();
     auto& transform = querier.get<components::Transform>(gLocalPlayer);
     auto& camera    = querier.get<components::Camera>(gLocalPlayer);
 
     // TODO: update postion in both camera and transfrom. forward vector could be get in camera.
-    transform.position += camera.forward * delta_time * move_speed;
+    transform.position += camera.forward * gDeltaTime * move_speed;
 }
 
 void MoveBackward() {
@@ -29,5 +27,5 @@ void MoveBackward() {
     auto& transform = querier.get<components::Transform>(gLocalPlayer);
     auto& camera    = querier.get<components::Camera>(gLocalPlayer);
 
-    transform.position -= camera.forward * delta_time * move_speed;
+    transform.position -= camera.forward * gDeltaTime * move_speed;
 }

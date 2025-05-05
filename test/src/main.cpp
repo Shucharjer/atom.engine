ec7ace70269be58be8cdbd93c95c906bc86f5e4e
Shucharjer <Shucharjer@outlook.com>
2025-05-05 18:37:17 +0800

    [test] add test case for framebuffer & post processing

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -12,6 +12,9 @@
 #include <btBulletDynamicsCommon.h>
 #include <easylogging++.h>
 #include <imgui.h>
+#include <stb_image.h>
+#include <stb_image_write.h>
+#include <winscard.h>
 #include <command.hpp>
 #include <core/langdef.hpp>
 #include <glm/ext/matrix_transform.hpp>
@@ -28,6 +31,7 @@
 #include "platform/path.hpp"
 #include "systems/render/BufferObject.hpp"
 #include "systems/render/Camera.hpp"
+#include "systems/render/Framebuffer.hpp"
 #include "systems/render/Model.hpp"
 #include "systems/render/ShaderProgram.hpp"
 #include "systems/render/Transform.hpp"
@@ -103,46 +107,111 @@ private:
     }
 };
 
-const auto CurrentPath    = atom::engine::io::CurrentPath();
-const auto ResourceFolder = CurrentPath / "resources";
-const auto ModelsFolder   = ResourceFolder / "models";
-const auto LoraPath       = ModelsFolder.string() + sep + "诺菈_by_幻塔" + sep + "诺菈.pmx";
-const auto MikuPath       = ModelsFolder.string() + sep + "miku" + sep + "model.pmx";
-const auto CubePath       = ModelsFolder.string() + sep + "Cube.fbx";
+const auto CurrentPath     = atom::engine::io::CurrentPath();
+const auto ResourceFolder  = CurrentPath / "resources";
+const auto ModelsFolder    = ResourceFolder / "models";
+const auto LoraPath        = ModelsFolder.string() + sep + "诺菈_by_幻塔" + sep + "诺菈.pmx";
+const auto MikuPath        = ModelsFolder.string() + sep + "miku" + sep + "model.pmx";
+const auto CubePath        = ModelsFolder.string() + sep + "Cube.fbx";
+const auto IllustriousPath = ModelsFolder.string() + sep + "illustrious" + sep + "illustrious.pmx";
 
-const auto VertShaderPath = ResourceFolder / "shaders" / "example.vert.glsl";
-const auto FragShaderPath = ResourceFolder / "shaders" / "example.frag.glsl";
+const auto ShaderPath               = ResourceFolder / "shaders";
+const auto VertShaderPath           = ShaderPath / "example.vert.glsl";
+const auto FragShaderPath           = ShaderPath / "example.frag.glsl";
+const auto PostprocessShaderPath    = ShaderPath / "post";
+const auto SimplyCopyVertShaderPath = PostprocessShaderPath / "copy.vert.glsl";
+const auto SimplyCopyFragShaderPath = PostprocessShaderPath / "copy.frag.glsl";
+const auto InverseVertShaderPath    = PostprocessShaderPath / "inverse.vert.glsl";
+const auto InverseFragShaderPath    = PostprocessShaderPath / "inverse.frag.glsl";
 
 auto& hub = hub::instance();
 auto& lib = ::hub.library<Model>();
 auto& tab = ::hub.table<Model>();
 Camera gCamera;
 ShaderProgram* gShaderProgram;
+ShaderProgram* gCopyShaderProgram;
+ShaderProgram* gInverseShaderProgram;
+
+enum class Postprocess : uint8_t {
+    None,
+    Inverse,
+    _reserve
+};
+
+Postprocess gPostprocess = Postprocess::None;
 
 static void ReloadShaderProgram() {
     delete gShaderProgram;
     gShaderProgram = new ShaderProgram(VertShaderPath, FragShaderPath);
 }
 
+static void SwitchPostProcessing() {
+    auto postprocessing = static_cast<uint8_t>(gPostprocess) + 1;
+    gPostprocess        = static_cast<Postprocess>(postprocessing);
+    if (gPostprocess == Postprocess::_reserve) {
+        gPostprocess = Postprocess::None;
+    }
+}
+
+ColorAttachment* gColorAttachment;
+Renderbuffer* gRenderbuffer;
+Framebuffer* gFramebuffer;
+VertexArrayObject* gScreenVAO;
+VertexBufferObject* gScreenVBO;
+
+const float gScreenVertices[] = {
+    // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+    -1.0f, 1.0f,  0.0f, 1.0f, //
+    -1.0f, -1.0f, 0.0f, 0.0f, //
+    1.0f,  -1.0f, 1.0f, 0.0f, //
+
+    -1.0f, 1.0f,  0.0f, 1.0f, //
+    1.0f,  -1.0f, 1.0f, 0.0f, //
+    1.0f,  1.0f,  1.0f, 1.0f  //
+};
+
 static void StartupSys(command& command, queryer& queryer) {
     gLocalPlayer = command.spawn<Transform, Camera>(Transform{}, Camera{});
     gWorld       = queryer.current_world();
 
-    auto entity = command.spawn<Model, Transform>(CubePath, Transform{});
+    auto entity = command.spawn<Model, Transform>(IllustriousPath, Transform{});
     auto& model = queryer.get<Model>(entity);
     auto proxy  = model.load();
     auto handle = lib.install(std::move(proxy));
     model.set_handle(handle);
     tab.emplace(model.path(), handle);
 
-    gShaderProgram = new ShaderProgram(VertShaderPath, FragShaderPath);
+    gShaderProgram        = new ShaderProgram(VertShaderPath, FragShaderPath);
+    gCopyShaderProgram    = new ShaderProgram(SimplyCopyVertShaderPath, SimplyCopyFragShaderPath);
+    gInverseShaderProgram = new ShaderProgram(InverseVertShaderPath, InverseFragShaderPath);
 
-    gCamera.position = math::Vector3(0.0F, 4.0F, -13.0F);
+    gCamera.position = math::Vector3(0.0F, -(2 << 2) + -(2 << 2), -(2 << 2) + -(2 << 2));
 
     auto& keyboard = KeyboardInput::instance();
     keyboard.setPressCallback(GLFW_KEY_O, &ReloadShaderProgram);
     keyboard.setPressCallback(GLFW_KEY_W, &MoveForward);
     keyboard.setPressCallback(GLFW_KEY_S, &MoveBackward);
+    keyboard.setPressCallback(GLFW_KEY_K, &SwitchPostProcessing);
+
+    gColorAttachment = new ColorAttachment(kDefaultWidth, kDefaultHeight);
+    gRenderbuffer    = new Renderbuffer(kDefaultWidth, kDefaultHeight);
+    gFramebuffer     = new Framebuffer();
+    gFramebuffer->bind();
+    gFramebuffer->attachColorAttachment(*gColorAttachment);
+    gFramebuffer->attachRenderbuffer(*gRenderbuffer);
+    if (auto status = gFramebuffer->getStatus(); status == GL_FRAMEBUFFER_COMPLETE) {
+        LOG(DEBUG) << "framebuffer completed";
+    }
+    else {
+        LOG(ERROR) << "framebuffer not completed: " << status;
+    }
+    gFramebuffer->unbind();
+
+    gScreenVAO = new VertexArrayObject();
+    gScreenVBO = new VertexBufferObject(sizeof(gScreenVertices));
+    gScreenVBO->set(static_cast<const float*>(gScreenVertices));
+    gScreenVBO->bind();
+    gScreenVAO->addAttributeForVertices2D(0, *gScreenVBO);
 }
 
 static void UpdateSys(command& command, queryer& queryer, float deltaTime) {
@@ -152,29 +221,56 @@ static void UpdateSys(command& command, queryer& queryer, float deltaTime) {
 
     const auto view = gCamera.view();
     const auto proj = gCamera.proj();
+
+    gFramebuffer->bind();
+    glEnable(GL_DEPTH_TEST | GL_STENCIL_TEST);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
     gShaderProgram->use();
     gShaderProgram->setMat4("view", view);
     gShaderProgram->setMat4("proj", proj);
     gShaderProgram->setVec3("viewPos", gCamera.position);
-
-    // gShaderProgram->setMat4("model", math::Mat4(1.0F));
-    // de_mesh->draw(*gShaderProgram);
 
     auto entities = queryer.query_all_of<Model, Transform>();
     for (const auto entity : entities) {
         auto& model            = queryer.get<Model>(entity);
         auto& transform        = queryer.get<Transform>(entity);
         math::Mat4 modelMatrix = transform.toMatrix();
-        auto handle            = model.get_handle();
-        auto proxy             = lib.fetch(handle);
-        for (const auto& mesh : proxy->meshes) {
-            if (mesh.visibility) {
-                // no scaling? or more?
-                gShaderProgram->setMat4("model", math::Mat4(1.0F));
-                mesh.draw(*gShaderProgram);
-            }
-        }
+        gShaderProgram->setMat4("model", modelMatrix);
+        auto handle = model.get_handle();
+        auto proxy  = lib.fetch(handle);
+        model.draw(lib, *gShaderProgram);
     }
+
+    // NOTE: the coordination starts at the left top corner in opengl, but the screen coordnation
+    // starts at the left buttom corner. If you want to get a readable picture, you should reverse
+    // these bytes.
+
+    // std::pmr::vector<std::byte> temp(1280 * 960 * 4);
+    // glReadPixels(0, 0, 1280, 960, GL_RGBA, GL_UNSIGNED_BYTE, temp.data());
+    // stbi_write_png("frame.png", 1280, 960, 4, temp.data(), 0);
+
+    gFramebuffer->unbind();
+
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    switch (gPostprocess) {
+        using enum Postprocess;
+    case Inverse: {
+        gInverseShaderProgram->use();
+        gInverseShaderProgram->setInt("screenTexture", 0);
+    } break;
+    case None:
+    default: {
+        gCopyShaderProgram->use();
+        gInverseShaderProgram->setInt("screenTexture", 0);
+    } break;
+    }
+    gScreenVAO->bind();
+    gColorAttachment->bind();
+    glDisable(GL_DEPTH_TEST);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glEnable(GL_DEPTH_TEST);
 }
 
 static void ShutdownSys(command& command, queryer& queryer) {
@@ -185,6 +281,13 @@ static void ShutdownSys(command& command, queryer& queryer) {
         LOG(INFO) << "unloading handle: [" << handle << "]";
         model.unload();
     }
+
+    delete gShaderProgram;
+    delete gColorAttachment;
+    delete gRenderbuffer;
+    delete gFramebuffer;
+    delete gCopyShaderProgram;
+    delete gInverseShaderProgram;
 }
 
 static void StartupPhysicsSystem(command& command, queryer& queryer) {

03574f8db3869745cd035f6920518d300df04ee0
Shucharjer <Shucharjer@outlook.com>
2025-05-11 01:17:48 +0800

    [test] create local player, record fps

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -104,11 +104,26 @@ auto& hub = hub::instance();
 auto& lib = ::hub.library<Model>();
 auto& tab = ::hub.table<Model>();
 
-static void StartupPhysicsSystem(command& command, queryer& queryer) {}
-
-static void UpdatePhysicsSystem(command& command, queryer& queryer, float deltaTime) {}
-
-static void ShutdownPhysicsSystem(command& command, queryer& queryer) {}
+static uint32_t highest = 0;
+static uint32_t lowest  = 1 << 31;
+static uint64_t total;
+static uint32_t seconds = 0;
+static uint32_t frame;
+static float accumulateTime = 0;
+static void updateFrame(command&, queryer&, float deltaTime) {
+    ++frame;
+    accumulateTime += deltaTime;
+    if (accumulateTime >= 1.0) {
+        ++seconds;
+        highest = std::max(highest, frame);
+        lowest  = std::min(lowest, frame);
+        total += frame;
+        LOG(INFO) << "current fps: " << frame << ", avg: " << (float)total / (float)seconds
+                  << "h: " << highest << "l: " << lowest;
+        accumulateTime = 0.0;
+        frame          = 0;
+    }
+}
 
 class ApplicationWindow : public Window {
 public:
@@ -118,9 +133,13 @@ public:
         // pConsole = factory.make<ConsolePanel>();
         // emplace("Console", pConsole);
         auto world = createWorld("main world");
+        world->add_startup(&CreateLocalPlayer);
+
         world->add_startup(StartupTestRenderSystem, atom::ecs::late_main_thread);
         world->add_update(UpdateTestRenderSystem, atom::ecs::late_main_thread);
         world->add_shutdown(ShutdownTestRenderSystem, atom::ecs::late_main_thread);
+
+        world->add_update(updateFrame);
 
         world->add_startup(StartupTestPhysicsSystem, atom::ecs::late_main_thread);
         world->add_update(UpdateTestPhysicsSystem, atom::ecs::late_main_thread);
